9cc1e100fb370495cc9e6454eb40c75c25d795b9
zjuadurey_laptop <adurey@DESKTOP-GQTPIVL.localdomain>
2023-01-02 16:23:17 +0800

    first commit

---
+++ PA0/hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	printf("Hello, linux world! 1\n");
+	printf("Hello, linux world! 2\n");
+	printf("Hello, linux world! 3\n");
+	printf("Hello, linux world! 4\n");
+	printf("Hello, linux world! 5\n");
+	printf("Hello, linux world! 6\n");
+	return 0;
+}
